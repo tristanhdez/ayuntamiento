0df700218f27bbe7f0b5f11bc219389d30460390
tristanhdez <christian.hflores@alumnos.udg.mx>
2022-05-31 03:08:41 -0500

    Modified UPDATE and LOGIN style

--- bd.py
+++ bd.py
@@ -4,5 +4,5 @@ import pymysql
 def obtener_conexion():
     return pymysql.connect(host='localhost',
                                 user='root',
-                                password='Password123*',
-                                db='townhall2')
+                                password='',
+                                db='')
